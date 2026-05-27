f45fbdf82da7a003dbbc5bc2eebb339e543641a3
prakharagarwal-07 <prakhar.agarwal.0710@gmail.com>
2026-05-27 23:23:51 +0530

    bs

--- binarysearch/binsearch.cpp
+++ binarysearch/binsearch.cpp
@@ -12,7 +12,7 @@ int binarysearch( vector<int>& array , int& target ){
 
     while( start <= end ){
 
-        int mid = ( start + end )/2 ;
+        int mid = start + (end-start)/2 ;
 
         if ( target < array[mid] ){ end = mid -1 ;}
         else if ( target > array[mid] ){ start = mid + 1 ; }
