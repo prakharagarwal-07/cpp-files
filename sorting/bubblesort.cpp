12b42f57024b75e75b78dd850948e093f0f7879e
prakharagarwal-07 <prakhar.agarwal.0710@gmail.com>
2026-06-19 19:57:13 +0530

    optimisation

--- sorting/bubblesort.cpp
+++ sorting/bubblesort.cpp
@@ -13,15 +13,23 @@ void sort( vector<int> &array ){
 
     for( i = 0 ; i < size ; i++ ){
 
+        bool isSwap = false;
+
         for( j = 0 ; j < size - i - 1 ; j++ ){
 
             if( array[j] > array[j+1]){
 
                 swap(array[j] , array[j+1]);
+                isSwap = true ;
             }
 
             
         }
+
+        if( isSwap = false ){
+            return;
+        }
+
     }
 
     
@@ -45,7 +53,7 @@ int main(){
     vector<int> array = { 4,1,5,2,3};
 
     sort( array ) ;
-    printarray(array);
+    printarray( array );
 
 
 
