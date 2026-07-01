6b1a48ce610c1c9cb953bb42d9cd06ec3bc8dd1a
prakharagarwal-07 <prakhar.agarwal.0710@gmail.com>
2026-07-01 19:45:52 +0530

    STL

--- STL/countbits_cp.cpp
+++ STL/countbits_cp.cpp
@@ -7,10 +7,13 @@ int main(){
 
     int n = 15;
     long int x = 12345;
+        long long int y = 123452345345;
+
 
 
     cout << __builtin_popcount(n) << endl;  //gives the number of 1 bits in n
-        cout << __builtin_popcount(x) << endl;  //used for long int
+        cout << __builtin_popcountl(x) << endl;  //used for long int
+        cout << __builtin_popcountll(y) << endl;  // for long long int
 
 
 
