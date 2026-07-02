175303d38819766d5786d4e4b4dd969659ddabe3
prakharagarwal-07 <prakhar.agarwal.0710@gmail.com>
2026-07-02 23:34:18 +0530

    string

--- strings/strings.cpp
+++ strings/strings.cpp
@@ -3,7 +3,25 @@
 using namespace std;
 int main(){
 
-    string str = " hello ";
+    string str = "hello ";  //dynamic in nature can be resized
 
-    cout << str;
+    string s1 = "hello ";  
+    string s2 = "world";
+
+    string s3 = s1 + s2;
+
+    cout << s3 << endl;
+
+    cout << str << endl;
+
+    cout << (s1 < s2) << endl ;   //checks lexicographically returns 1 or 0 for true and false
+
+    cout << s1.length() << endl;
+
+
+    string str1;
+
+    getline(cin , str1);
+
+    cout << str1 << endl;
 }
