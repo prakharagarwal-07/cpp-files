728a81f971e125541ebe1a3a8c6d33f73fc05ed9
prakharagarwal-07 <prakhar.agarwal.0710@gmail.com>
2026-07-03 18:29:15 +0530

    string

--- strings/xtrafxns.cpp
+++ strings/xtrafxns.cpp
@@ -5,15 +5,16 @@ int main(){
 
     char c = 'A';
 
-    cout << isupper(c) << endl;
+    cout << isupper(c) << endl;  //checks if uppercase
 
-    c = tolower(c);
+
+    c = tolower(c);   //decapitalises the upper char
 
     cout << c << endl;
 
     char x = '#';
 
-    cout << isalnum(x);
+    cout << isalnum(x);  //checks if the given char is alpha numeric or not
 
 
 }
