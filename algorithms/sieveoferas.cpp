c116f1c551de9a08529ae514ef2cd5eb16709a20
prakharagarwal-07 <prakhar.agarwal.0710@gmail.com>
2026-07-12 23:17:44 +0530

    sieve of eras

---
+++ algorithms/sieveoferas.cpp
@@ -0,0 +1,54 @@
+#include <iostream>
+#include <vector>
+using namespace std; 
+
+
+int countPrimes(int &n) {
+
+        int i;
+
+        vector<bool> isPrime( n , true );
+
+
+        int count = 0;
+        
+
+        for( i = 2 ; i < n ; i++ ){
+
+            if( isPrime[i] ){
+
+                count++;
+
+                for(int j = i+i ; j < n ; j = j+i ){
+
+                    isPrime[j] = false;
+                }
+            }
+
+
+        }
+
+        return count;
+        
+    }
+
+int main(){
+
+    int n = 50;
+
+    cout << countPrimes( n );
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
