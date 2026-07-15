8bec83caa4b5fbbccfc12425aefdf3a79d236966
prakharagarwal-07 <prakhar.agarwal.0710@gmail.com>
2026-07-15 23:32:17 +0530

    changes

--- recursion/basic.cpp
+++ recursion/basic.cpp
@@ -1,22 +1,23 @@
-#include <iostream>
-using namespace std;
+    #include <iostream>
+    using namespace std;             //space complexity includes ---> the call stack depth * memory in each call
 
-void printNum(int n){
+    void printNum(int n){
 
-    if( n == 1 ){
+        if( n == 1 ){
 
-        cout << 1 << " ";
-        return;
+            cout << 1 << " ";   // this is called base case
+            return;
+        }
+
+        cout << n << " ";
+
+        printNum(n-1);             //recursion as the function calls itself
+
     }
+    int main(){
 
-    cout << n << " ";
-
-    printNum(n-1);
-}
-int main(){
-
-    printNum( 10 );
+        printNum( 10 );
 
 
 
-}
+    }
